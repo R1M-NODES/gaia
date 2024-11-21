8ee0a46a0f742cac76472c35a23616a71425183a
Sorkand <96412234+sorkand1@users.noreply.github.com>
2024-11-21 12:16:55 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo apt update -y && sudo apt-get update -y
 
 curl -sSfL 'https://github.com/GaiaNet-AI/gaianet-node/releases/latest/download/install.sh' | bash
 source ~/.bashrc
-gaianet init --config https://raw.githubusercontent.com/R1M-NODES/gaia/master/config.json
+gaianet init --config https://raw.githubusercontent.com/GaiaNet-AI/node-configs/main/qwen2-0.5b-instruct/config.json
 gaianet start
 gaianet info
 
