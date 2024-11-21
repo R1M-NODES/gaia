414396d59cd0b71743ab13e1ef4b5db962898408
Sorkand <96412234+sorkand1@users.noreply.github.com>
2024-11-21 11:37:13 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+source <(curl -s https://raw.githubusercontent.com/R1M-NODES/utils/master/common.sh)
+printLogo
+
+sudo apt update -y && sudo apt-get update -y
+
+curl -sSfL 'https://github.com/GaiaNet-AI/gaianet-node/releases/latest/download/install.sh' | bash
+source ~/.bashrc
+gaianet init --config https://raw.githubusercontent.com/GaiaNet-AI/node-configs/main/qwen2-0.5b-instruct/config.json
+gaianet start
+gaianet info
+
+sudo bash -c "cat > /etc/systemd/system/gaianet.service" <<EOL
+[Unit]
+Description=Gaianet Node Service
+After=network.target
+
+[Service]
+Type=forking
+RemainAfterExit=true
+ExecStart=/root/gaianet/bin/gaianet start
+ExecStop=/root/gaianet/bin/gaianet stop
+ExecStopPost=/bin/sleep 20
+Restart=always
+RestartSec=5
+User=root
+
+[Install]
+WantedBy=multi-user.target
+EOL
+
+sudo systemctl daemon-reload
+sudo systemctl restart gaianet.service
+sudo systemctl enable gaianet.service
+sudo systemctl status gaianet.service
+
